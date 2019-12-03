a57d189ea2dc26d6b4b4f0b5f30b14b92eb747ff
Илья Негруб <neinik@mail.ru>
2019-12-03 15:39:38 +0300

    ^

--- CodeCpp/BitmapFilterRed.cpp
+++ CodeCpp/BitmapFilterRed.cpp
@@ -5,7 +5,7 @@ Color BitmapFilterRed::UpdateColor(Color color)
 	Color color_out;
 
 	color_out.A = color.A;
-	color_out.R = 0xFF - color.R;
+	color_out.R = 0xFF ^ color.R;
 	color_out.G = 0;
 	color_out.B = 0;
 
